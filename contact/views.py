c06ba3e5398ccda339b0778efe8359a13fdd535e
An Vu <vungocan0312@gmail.com>
2021-12-29 13:04:28 +1300

    Revert "Update contact button"
    This reverts commit affdaf0ff393a419c23e1cc0afb98eb3ece600d8.

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import ContactForm
 
 
-
+def contact_view(request):
+    form = ContactForm()
+    context = {'form': form}
+    return render(request, 'contact/contact.html', context)
 # Create your views here.
--- mysite/views.py
+++ mysite/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import ContactForm
 
 def index(request):
     return render(request=request, template_name="index.html")
-
-
-def contact_view(request):
-    form = ContactForm()
-    context = {'form': form}
-    return render(request, 'contact/contact.html', context)
