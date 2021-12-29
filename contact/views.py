b2802178fead8692beba274019d0871ac8bb18af
An Vu <vungocan0312@gmail.com>
2021-12-29 13:15:22 +1300

    reversed

--- contact/views.py
+++ contact/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
-from .forms import ContactForm
 
-<<<<<<< HEAD
+
+
 def contact_view(request):
     return HttpResponse("Contact app works!")
-=======
 
-def contact_view(request):
-    form = ContactForm()
-    context = {'form': form}
-    return render(request, 'contact/contact.html', context)
->>>>>>> parent of affdaf0 (Update contact button)
-# Create your views here.
+
